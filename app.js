ff1e9edb8af19365a7644b9eeeeb361496c96aad
vikiade00 <vikiade00@gmail.com>
2024-11-27 15:30:56 +0700

    fix cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const db = require("./config/db");
-const cors = require(cors);
+const cors = require("cors");
 const barangRoutes = require("./routes/BarangRoutes");
 
 const app = express();
